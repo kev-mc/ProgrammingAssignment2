6aa7135ecbf080f5943e275af8a8359c7d6a0df3
kev-mc <kev-mc@users.noreply.github.com>
2015-11-22 22:36:24 +1300

    Completed assignment
    Added functionality to cache a matrix and its inverse, using a list as a
    wrapper object exposing getter and setter functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+## Create a list that acts as a wrapper object to cache a matrix and its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  # initial value of the inverse is null
+  inv <- NULL
+  
+  # resetting the matrix needs to also reset the inverse to its inital (NULL) value
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  # basic get function for the original matrix
+  get <- function() x
+  
+  # getter/setter functions for the cached inverse value
+  setinverse <- function(i) inv <<- i
+  getinverse <- function() inv
+  
+  # create (and return) a list that exposes the getter and setter functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Find the inverse of a matrix, returning a cached value if available, otherwise calculating and caching the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # try and retreive the cahched value
+  i <- x$getinverse()
+  
+  if(!is.null(i)) {
+    # the inverse has already been calculated, so return it
+    return(i)
+  }
+  
+  # retrieve the underlying matrix from the list
+  mtx <- x$get()
+  
+  # calculate its inverse
+  i <- solve(mtx, ...)
+  
+  # cache the calculated value
+  x$setinverse(i)
+  
+  #return the result
+  i
 }
